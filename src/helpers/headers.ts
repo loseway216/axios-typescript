5048b9f2a026fd908c6d98b3b43e38f84d9fd3e4
loseway216 <loseway216@gmail.com>
2023-11-17 20:23:25 +0800

    feat: add headers function

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,5 +1,17 @@
 import { isPlainObject } from './util'
 
+function normalizeHeaderName(headers: any, normalizedName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
 export function processHeaders(headers: any, data: any): any {
   if (isPlainObject(data)) {
     if (headers && !headers['Content-Type']) {
