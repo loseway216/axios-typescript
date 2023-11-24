3ff507b28996774671fb8003bb3265877598633e
loseway216 <loseway216@gmail.com>
2023-11-24 14:37:08 +0800

    feat: add default config

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -13,9 +13,12 @@ interface PromiseChain<T> {
 }
 
 export default class Axios {
+  defaults: AxiosRequestConfig
   interceptors: Interceptors
 
-  constructor() {
+  constructor(initConfig: AxiosRequestConfig) {
+    this.defaults = initConfig
+
     this.interceptors = {
       request: new InterceptorManager<AxiosRequestConfig>(),
       response: new InterceptorManager<AxiosPromise>()
